ba4c3e8ac761fe886d699923c556768d8f566917
satyam kumar <145699513+SATAYM8540@users.noreply.github.com>
2025-09-17 15:57:50 +0530

    Update AdminDashboard.jsx

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -7,7 +7,7 @@
 // import * as XLSX from "xlsx";
 // import { saveAs } from "file-saver";
 
-// const socket = io("http://localhost:5000");
+// const socket = io("https://employee-tracker-5.onrender.com");
 
 // export default function AdminDashboard() {
 //   const [employees, setEmployees] = useState([]);
