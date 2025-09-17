8d697cc611367290de711f16418d7c5aa8e87cc2
satyam8540 <satyamkumar00049@gmail.com>
2025-09-17 16:49:38 +0530

    update axios

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,24 @@
+// import axios from "axios";
+
+// const client = axios.create({
+//   baseURL: "/api"
+// });
+
+// client.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// export default client;
+
+
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 const client = axios.create({
-  baseURL: "/api"
+  baseURL: API_URL
 });
 
 client.interceptors.request.use((config) => {
