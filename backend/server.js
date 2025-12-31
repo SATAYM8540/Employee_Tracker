c18bd1eb3be070e2d5335d5685946e16dfbb5f95
satyam8540 <satyamkumar00049@gmail.com>
2025-12-31 15:33:02 +0530

    fixed env

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,14 @@ import attendanceRoutes from "./routes/attendanceRoutes.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    `${process.env.FRONTEND_URL} || http://localhost:5173`,          
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json({ limit: "50mb" }));
 
 connectDB();
